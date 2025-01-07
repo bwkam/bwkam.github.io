e79fb88cfd3102c05d2f3334b0f17a5f7eaf82e7
bwkam <userdev987@gmail.com>
2025-01-07 18:39:27 +0200

    center blog post

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./templates/*.html"],
-  theme: {
-    extend: {
-	fontFamily: {
-	    dm: ["Dank Mono", "Consolas", "Courier New", "Courier", "monospace"]
-	},
-    },
+  theme: {},
   plugins: [],
 }
 
